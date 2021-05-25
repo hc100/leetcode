29034b2a904cd9eaaad0a6aba4e7f21b19ccb013
Kyoichi Ozaki <kyoichi@gmail.com>
2021-05-25 23:58:49 +0900

    💮 678. Valid Parenthesis String の解答

--- valid-parenthesis-string/main.go
+++ valid-parenthesis-string/main.go
@@ -3,5 +3,35 @@ package main
 import ()
 
 func checkValidString(s string) bool {
-	return true
+	lo, hi := 0, 0
+	lo, hi = possibleRange(lo, hi, s)
+
+	return lo == 0
+}
+
+func possibleRange(lo int, hi int, s string) (int, int) {
+	for _, r := range s {
+		c := string(r)
+		if c == "(" {
+			lo = lo + 1
+		} else {
+			lo = lo - 1
+		}
+
+		if c != ")" {
+			hi = hi + 1
+		} else {
+			hi = hi - 1
+		}
+
+		if hi < 0 {
+			return lo, hi
+		}
+
+		if lo < 0 {
+			lo = 0
+		}
+	}
+
+	return lo, hi
 }
